11040f7e8423d9aac89825f255ab7f66308c269e
Roy Perlman <40452532+roy-is-our-boy13@users.noreply.github.com>
2020-03-05 17:00:26 -0800

    Twitter Like App

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Roy Perlman on 3/5/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell
+{
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetConent: UILabel!
+    
+    
+    
+    
+    
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
